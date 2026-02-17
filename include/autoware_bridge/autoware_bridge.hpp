e1265ed3d4cb09469663fa4169a77adba0b48252
Hideyasu.Miyagi <hdmiyagi@gmail.com>
2026-02-17 16:25:00 +0900

    Removed unnecessary code that tries to filter control_md topic

--- include/autoware_bridge/autoware_bridge.hpp
+++ include/autoware_bridge/autoware_bridge.hpp
@@ -15,7 +15,6 @@
 #include <std_msgs/msg/bool.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <tier4_system_msgs/msg/mode_change_available.hpp>
-#include <autoware_auto_vehicle_msgs/msg/control_mode_report.hpp>
 #include <atomic>
 #include <memory>
 #include <mutex>
@@ -39,13 +38,13 @@ private:
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr autonomous_driving_request_subscription_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr cancel_task_subscription_;
   rclcpp::Subscription<ModeChangeAvailable>::SharedPtr localization_quality_subscriber_;
-  rclcpp::Subscription<autoware_auto_vehicle_msgs::msg::ControlModeReport>::SharedPtr control_mode_subscription_;
+  //rclcpp::Subscription<autoware_auto_vehicle_msgs::msg::ControlModeReport>::SharedPtr control_mode_subscription_;
 
   // ROS2 Publishers
   rclcpp::Publisher<diagnostic_msgs::msg::KeyValue>::SharedPtr task_response_publisher_;
   rclcpp::Publisher<diagnostic_msgs::msg::KeyValue>::SharedPtr cancel_response_publisher_;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr reinitialize_response_publisher_;
-  rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::ControlModeReport>::SharedPtr control_mode_publisher_;
+  //rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::ControlModeReport>::SharedPtr control_mode_publisher_;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr suppress_autonomous_publisher_;
 
   // ROS2 Services
@@ -67,7 +66,6 @@ private:
   void cancelTaskCallback(const std_msgs::msg::String::SharedPtr msg);
   void onTimerCallback();
   void localizationQualityCallback(const ModeChangeAvailable & msg);
-  void controlModeReportCallback(const autoware_auto_vehicle_msgs::msg::ControlModeReport::SharedPtr msg);
 
   // Helper functions
   bool isTaskRejected(const std::string & task_name);
